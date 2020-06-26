49360c7fe6c1eb1a01a2bffd4607c89b62824998
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-06-25 17:10:54 -0700

    Table view of currently playing movies from The Movie DB API

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Kaitlyn Gunadhi on 6/25/20.
+//  Copyright © 2020 Kaitlyn Gunadhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
