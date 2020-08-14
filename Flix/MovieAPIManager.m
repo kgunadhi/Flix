23caad278e05241d747076a57a9cab7ee48f675a
Kaitlyn Gunadhi <kgunadhi@fb.com>
2020-08-14 07:33:04 -0700

    Fix

--- Flix/MovieAPIManager.m
+++ Flix/MovieAPIManager.m
@@ -26,7 +26,7 @@
 }
 
 - (void)fetchNowPlaying:(void(^)(NSArray *movies, NSError *error))completion {
-    NSURL *const url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key="];
+    NSURL *const url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (error != nil) {
